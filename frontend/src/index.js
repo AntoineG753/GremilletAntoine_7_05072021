be1ba63e4243e64042f2170947a1bf80dca46e81
antoine <antoine.gremillet34@gmail.com>
2021-08-03 12:29:39 +0200

    init du FRONT
    initialisation du front + rajout du favicon et du titre de la page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<React.StrictMode> <App /> </React.StrictMode>,document.getElementById('root')); // on dit que le app dans app.js vas s'afficher dans la class root du index.html
-
+ReactDOM.render(<React.StrictMode> <App /> </React.StrictMode>,document.getElementById('root'));  // on dit que le app dans app.js vas s'afficher dans la class root du index.html
+ 
 reportWebVitals();
